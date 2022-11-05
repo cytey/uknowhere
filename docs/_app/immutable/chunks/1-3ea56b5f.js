530ee3fe2a83e72a2b7de14d2988da5e4cd169ec
Junhong <teyjunhong@gmail.com>
2022-11-05 19:35:57 +0800

    Add: Arrow controls and fix mobile

--- docs/_app/immutable/chunks/1-3ea56b5f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-4f584c5c.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-ab4d4c3f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-649e89e5.js";export{t as component};
